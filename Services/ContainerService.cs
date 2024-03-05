53ef9a937b9ba053502495fb7857a5dada021d7a
Dharmesh Singh <dharmesh.singh@hcl.com>
2024-03-05 07:31:43 +0530

    Add BlobClientServices

---
+++ Services/ContainerService.cs
@@ -0,0 +1,34 @@
+﻿
+using Azure.Storage.Blobs;
+
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        private readonly BlobServiceClient _blobClient;
+
+        public ContainerService(BlobServiceClient blobServiceClient)
+        {
+            _blobClient = blobServiceClient;
+        }
+        public Task CreateConatinerAsync(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteContainerAsync(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainersAndBlobAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IContainerService.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureBlobProject.Services
+{
+    public interface IContainerService
+    {
+        Task<List<string>> GetAllContainersAndBlobAsync();
+        Task<List<string>> GetAllContainersAsync();
+        Task CreateConatinerAsync(string containerName);
+        Task DeleteContainerAsync(string containerName);
+    }
+}
